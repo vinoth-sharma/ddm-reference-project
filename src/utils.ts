c32d384b60f65bec4a7d5442d0931d58bf45762e
Nithya Murali <Nithya.Murali@mu-sigma.com>
2018-12-27 20:39:11 +0530

    removed console msg

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,6 @@ export default class Utils {
 
     setTimeout(() => {
       for (let i = 0; i < modals.length; i++) {
-        console.log('in for', modals[i], modals[i].querySelector('.close'));
         (<HTMLElement>modals[i].querySelector('.close')).click();
       }
     });

548b6a93aa5805fb8caa11e48410a6f2f532db00
Nithya Murali <Nithya.Murali@mu-sigma.com>
2019-01-11 19:47:55 +0530

    added reports nav-bar

---
+++ src/app/reports-navbar/reports-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reports-navbar',
+  templateUrl: './reports-navbar.component.html',
+  styleUrls: ['./reports-navbar.component.css']
+})
+
+export class ReportsNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}

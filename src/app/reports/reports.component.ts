17ab115451e2a495047f3d70eefe14db20b041c6
Nithya Murali <Nithya.Murali@mu-sigma.com>
2019-01-18 15:50:42 +0530

    fixed joins-help option component

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -5,11 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './reports.component.html',
   styleUrls: ['./reports.component.css']
 })
+
 export class ReportsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }

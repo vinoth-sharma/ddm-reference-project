7a8e0dbad510127aa048523884aedebc6a3ef085
Nithya Murali <Nithya.Murali@mu-sigma.com>
2019-01-28 13:59:13 +0530

    fixed some indentation

--- src/app/shared-components/inline-edit/inline-edit.component.ts
+++ src/app/shared-components/inline-edit/inline-edit.component.ts
@@ -5,30 +5,29 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './inline-edit.component.html',
   styleUrls: ['./inline-edit.component.css']
 })
+
 export class InlineEditComponent implements OnInit {
-  
+
   @Input() item: any;
-  @Input() itemID:any;
+  @Input() itemID: any;
   @Input() customClass: string = '';
 
   @Output() public onSave = new EventEmitter();
-  
-  isReadOnly = true;  
 
-  ngOnInit() {
-    
-  }
+  isReadOnly = true;
+
+  ngOnInit() { }
 
   onDblClick() {
     this.isReadOnly = !this.isReadOnly;
   }
 
-  onBlur(){
+  onBlur() {
     this.isReadOnly = true;
   }
 
-  onKeyDown(item,tableID,tableName){
-      this.onSave.emit({old_val:item,table_id:tableID,table_name:tableName});
-      this.isReadOnly = true;
+  onKeyDown(item, tableID, tableName) {
+    this.onSave.emit({ old_val: item, table_id: tableID, table_name: tableName });
+    this.isReadOnly = true;
   }
 }

15863c845afa53a0f136f99087761c880c7d1473
Nithya Murali <Nithya.Murali@mu-sigma.com>
2019-02-11 17:57:51 +0530

    resolved conflicts

--- src/app/shared-components/sidebars/object-explorer-sidebar/object-explorer-sidebar.service.ts
+++ src/app/shared-components/sidebars/object-explorer-sidebar/object-explorer-sidebar.service.ts
@@ -54,6 +54,12 @@ export class ObjectExplorerSidebarService {
       .pipe(catchError(this.handleError));
   }
 
+  public saveCustomTableName(options) {
+    let serviceUrl = `${environment.baseUrl}semantic_layer/manage_views/`;
+    return this.http.put(serviceUrl, options)
+      .pipe(catchError(this.handleError));
+  }
+
   public listValues(options) {
     let viewUrl = `${environment.baseUrl}semantic_layer/get_list_of_values/?table_name=${options.tableId}&column_name=${options.columnName}`;
 
@@ -62,12 +68,12 @@ export class ObjectExplorerSidebarService {
   };
 
   public colProperties(options) {
-    
+
     let colUrl = `${environment.baseUrl}semantic_layer/column_properties/`
     let requestBody = new FormData();
     requestBody.append('table_id', options.tableId);
     requestBody.append('column_name', options.columnName);
-    return this.http.post(colUrl,requestBody)
+    return this.http.post(colUrl, requestBody)
       .pipe(
         catchError(this.handleError)
       )
@@ -83,19 +89,19 @@ export class ObjectExplorerSidebarService {
       .pipe(catchError(this.handleError));
   };
 
-  public checkUnique(){
+  public checkUnique() {
     let serviceUrl = `${environment.baseUrl}semantic_layer/update_semantic_layer/`;
     return this.http.get(serviceUrl)
       .pipe(catchError(this.handleError));
   };
 
-  public updateSemanticName(options){
+  public updateSemanticName(options) {
     let serviceUrl = `${environment.baseUrl}semantic_layer/update_semantic_layer/`;
-    return this.http.put(serviceUrl,{
-      "sl_id" : options.slId,
-      "new_semantic_layer_name" : options.new_semantic_layer ,
-      "old_semantic_layer_name" : options.old_semantic_layer
-      
+    return this.http.put(serviceUrl, {
+      "sl_id": options.slId,
+      "new_semantic_layer_name": options.new_semantic_layer,
+      "old_semantic_layer_name": options.old_semantic_layer
+
     })
       .pipe(catchError(this.handleError));
   };
@@ -125,7 +131,15 @@ export class ObjectExplorerSidebarService {
     let data = {
       'sl_tables_id': selectedTables
     }
+    return this.http.request('delete', deleteUrl, { body: data })
+      .pipe(catchError(this.handleError));
+  }
 
+  public deleteCustomTables(selectedTables: any) {
+    let deleteUrl = `${environment.baseUrl}semantic_layer/manage_views/`;
+    let data = {
+      'custom_table_id': selectedTables
+    }
     return this.http.request('delete', deleteUrl, { body: data })
       .pipe(catchError(this.handleError));
   }
@@ -148,6 +162,20 @@ export class ObjectExplorerSidebarService {
     let deleteUrl = `${environment.baseUrl}semantic_layer/update_semantic_layer/`;
 
     return this.http.request('delete', deleteUrl, { body: data })
+      .pipe(catchError(this.handleError));
+  }
+
+  public deleteColumn(column: any) {
+    let deleteUrl = `${environment.baseUrl}semantic_layer/manage_tables/`;
+
+    return this.http.request('delete', deleteUrl, { body: column })
+      .pipe(catchError(this.handleError));
+  }
+
+  public addColumn(data: any) {
+    let addUrl = `${environment.baseUrl}semantic_layer/calculated_column_custom_table/`;
+
+    return this.http.post(addUrl, data)
       .pipe(catchError(this.handleError));
   }
 }

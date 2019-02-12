4cdeaabe855b827c1aa0b9c3926d63236913cd8f
Nithya Murali <Nithya.Murali@mu-sigma.com>
2019-02-12 14:14:11 +0530

    removed comments

--- src/app/semantic-existing/semantic-existing.component.ts
+++ src/app/semantic-existing/semantic-existing.component.ts
@@ -33,48 +33,15 @@ export class SemanticExistingComponent implements OnInit {
       })
   };
 
-  // public print() {
-  //   const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-  //   const EXCEL_EXTENSION = '.xlsx';
-
-  //   const table = document.getElementById('semantic-layers');
-  //   const workbook = XLSX.utils.table_to_book(table);
-  //   const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-  //   const data: Blob = new Blob([excelBuffer], { type: EXCEL_TYPE });
-
-  //   FileSaver.saveAs(data, 'semantic-layers-' + new Date().getTime() + EXCEL_EXTENSION);
-  // }
-
-
   public print() {
     const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
     const EXCEL_EXTENSION = '.xlsx';
 
     const table = document.getElementById('semantic-layers');
-    const worksheet = XLSX.utils.table_to_sheet(table);
+    const workbook = XLSX.utils.table_to_book(table);
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    const data: Blob = new Blob([excelBuffer], { type: EXCEL_TYPE });
 
-    const workbook = XLSX.utils.book_new();
-    
-    
-    worksheet.B4.s = {
-      font: {
-        name: "Arial", 
-        bold: true,
-        underline: true,
-        color: {rgb: "FF000000"}
-      }
-    }
-    
-    console.log('print', worksheet, worksheet.B4, worksheet.B4.v, worksheet.B4.s)
-
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-
-    // const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-    // const data: Blob = new Blob([excelBuffer], { type: EXCEL_TYPE });
-
-    // FileSaver.saveAs(data, 'semantic-layers-' + new Date().getTime() + EXCEL_EXTENSION);
-
-    XLSX.writeFile(workbook, 'semantic-layers-' + new Date().getTime() + EXCEL_EXTENSION);
-
+    FileSaver.saveAs(data, 'semantic-layers-' + new Date().getTime() + EXCEL_EXTENSION);
   }
 }

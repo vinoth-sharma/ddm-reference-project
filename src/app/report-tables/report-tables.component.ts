0d7d32bdf02b3d7b580265c4a643e61e0acc4b1e
Nithya Murali <Nithya.Murali@mu-sigma.com>
2019-02-22 16:26:56 +0530

    updated releatedTables API

--- src/app/report-tables/report-tables.component.ts
+++ src/app/report-tables/report-tables.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from "ngx-toastr";
-import { SemdetailsService } from '../semdetails.service'
+// import { SemdetailsService } from '../semdetails.service'
+import { ObjectExplorerSidebarService } from '../shared-components/sidebars/object-explorer-sidebar/object-explorer-sidebar.service';
 import { ReportsService } from "../reports/reports.service";
 
 @Component({
@@ -16,18 +17,21 @@ export class ReportTablesComponent implements OnInit {
   relatedTables: any[];
   cachedRelatedTables: any[];
   // isLoading: boolean;
-  defaultError:string = "There seems to be an error. Please try again later.";
-  join:string = '';
+  defaultError: string = "There seems to be an error. Please try again later.";
+  join: string = '';
+  primaryKey: string = '';
+  foreignKey: string = '';
+  checked={'tables':{}, 'relatedTables': {}};
 
-  constructor(private semdetailsService: SemdetailsService, private reportsService: ReportsService, private toasterService: ToastrService) { }
+  constructor(private objectExplorerSidebarService: ObjectExplorerSidebarService, private reportsService: ReportsService, private toasterService: ToastrService) { }
 
   ngOnInit() {
-    // this.isLoading = true;
-    this.semdetailsService.myMethod$.subscribe(response => {
-      this.tables = response || [];
+    this.objectExplorerSidebarService.getTables.subscribe(tables => {
+      console.log('semdetails', tables)
+      this.tables = tables || [];
       this.cachedTables = this.tables.slice();
       // this.isLoading = false;
-    });
+    })
   }
 
   public getRelatedTables(tableId: number) {
@@ -35,10 +39,10 @@ export class ReportTablesComponent implements OnInit {
       this.relatedTables = response['table_data'] || [];
       this.cachedRelatedTables = this.relatedTables.slice();
     },
-    error => {
-      this.toasterService.error(error.message["error"] || this.defaultError);
-      this.relatedTables = [];
-    });
+      error => {
+        this.toasterService.error(error.message["error"] || this.defaultError);
+        this.relatedTables = [];
+      });
   }
 
   public getFilteredList(searchText: string, side: string) {
@@ -65,6 +69,32 @@ export class ReportTablesComponent implements OnInit {
     }
     else {
       return tables;
+    }
+  }
+
+  public setJoinInfo(table: any){
+    this.join = table['type_of_join'];
+    // TODO: where to?
+    this.primaryKey = table['keys']['p_key']['primary_key'];
+    this.foreignKey = table['keys']['f_key']['foreign_key'];
+
+    console.log('setJoin', table, this.join, this.primaryKey, this.foreignKey)
+
+  }
+
+  setChecked(column, event){
+    event.stopPropagation();
+    // this.checked[column+'_'+index] = !this.checked[column+'_'+index]
+    // console.log('setCol', column, event, this.checked);
+  }
+
+  setKey(column:string, side: string){
+    console.log('setKey', column, side)
+    if(side === 'left'){
+      this.primaryKey = column;
+    }
+    else if(side === 'right'){
+      this.foreignKey = column;
     }
   }
 }

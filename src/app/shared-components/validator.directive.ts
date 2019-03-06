8478ddc6a02747c443f673452d905298f8fa9058
Sahana P <sonal.raut@gm.com>
2019-03-06 18:56:43 +0530

    Addressed comments

--- src/app/shared-components/validator.directive.ts
+++ src/app/shared-components/validator.directive.ts
@@ -1,22 +1,20 @@
-import { Directive, ElementRef, HostListener, SimpleChanges, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appValidator]'
 })
 export class ValidatorDirective {
 
-  @Input() validatorType:string;
-
   splCharRegex = /[~`!#$%\^&*+=\-\[\]\/';,/{}|\\":<>\?@]/;
 
   constructor(private el: ElementRef) { }
 
-  @HostListener('input')
-  onInput(){
-    if(this.validatorType == 'splChar'){
-      if(new RegExp(this.splCharRegex).test((this.el.nativeElement.value)))
-        this.el.nativeElement.value = this.el.nativeElement.value.replace(/[^A-Za-z0-9]/g, '').replace(/\s/g,'');
-    }
+  @HostListener('input') onInput(){
+      this.validate();
   }
 
+  private validate(){
+    if(new RegExp(this.splCharRegex).test(this.el.nativeElement.value))
+        this.el.nativeElement.value = this.el.nativeElement.value.replace(/[^A-Za-z0-9]/g, '').replace(/\s/g,'');
+  }
 }

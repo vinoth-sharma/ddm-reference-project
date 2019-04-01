09e794d414810a16469642bb3b281f69c33e2bf0
Himal Mangla <himal.mangla@gm.com>
2019-04-01 10:32:53 +0530

    Adding CSS changes and removing old code from the ts file

--- src/app/create-report/create-report-layout/create-report-layout.component.ts
+++ src/app/create-report/create-report-layout/create-report-layout.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, NavigationEnd } from "@angular/router";
-import * as $ from "jquery";
-
-import { SharedDataService } from "../shared-data.service";
 
 @Component({
   selector: 'app-create-report-layout',
@@ -14,23 +10,9 @@ export class CreateReportLayoutComponent implements OnInit {
 
   show: boolean;
 
-  constructor(private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private sharedDataService: SharedDataService) { }
+  constructor() { }
 
   ngOnInit() {
-    // TODO: jquery 
-    $("#sidebar").toggleClass("active");
-
-    this.sharedDataService.setSelectedTables([]);
-  }
-
-  public showNav() {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.show = (this.activatedRoute.snapshot['firstChild']['url'][0]['path'] !== 'select-tables') ? true : false;
-      }
-    });
   }
 
 }

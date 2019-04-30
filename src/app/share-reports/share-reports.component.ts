f0de61e0696b3825e8366422b6809ac72839b1fd
bz1ksc <aneesha.biju@gm.com>
2019-04-30 22:23:29 +0530

    implemented upload image

--- src/app/share-reports/share-reports.component.ts
+++ src/app/share-reports/share-reports.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import  { froalaEditor} from froala
+// import * as froalaEditor from 'froala-editor/js/froala_editor.pkgd.min.js';
 
 @Component({
   selector: 'app-share-reports',
@@ -7,24 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShareReportsComponent implements OnInit {
 
-  public shareData:any = {};
-  public formats:any = [];
-  public deliveryMethods:any = [];
-  public sheetList:any = [];
-  public isSheets:boolean;
-  public isSignature:boolean;
+  public shareData: any = {};
+  public formats: any = [];
+  public deliveryMethods: any = [];
+  public sheetList: any = [];
+  public isSheets: boolean;
+  public isSignature: boolean;
+  public imagePath;
+  imgUrl: any;
+  imgPreview: any;
+  file: File;
 
   constructor() { }
 
   ngOnInit() {
     this.initialState();
+    // $(function() {
+    //   $('textarea#froala-editor').froalaEditor()
+    // });
   }
 
   /**
    * initialState
    */
   public initialState() {
-    this.shareData =  {
+    this.shareData = {
       'reportName': '',
       'reportType': 'Entire Report',
       'format': '',
@@ -34,7 +43,7 @@ export class ShareReportsComponent implements OnInit {
           'eUsers': '',
           'eView': false,
           'eDesc': '',
-          'eSignature':''
+          'eSignature': ''
         },
         'ftp': {
           'ftpAdrs': '',
@@ -48,8 +57,8 @@ export class ShareReportsComponent implements OnInit {
       }
     };
 
-    this.formats = ['Csv','Excel','Pdf'];
-    this.deliveryMethods = ['Email','FTP'];
+    this.formats = ['Csv', 'Excel', 'Pdf'];
+    this.deliveryMethods = ['Email', 'FTP'];
     this.isSheets = false;
     this.isSignature = false;
   };
@@ -57,18 +66,39 @@ export class ShareReportsComponent implements OnInit {
   /**
    * shareReport
    */
-  public shareReport() {};
+  public shareReport() {
+
+  };
 
   /**
    * autoSize
    */
   public autoSize(el) {
     let element = el;
-    setTimeout(function(){
+    setTimeout(function () {
       element.style.cssText = 'height:auto;';
-      let height = element.scrollHeight+5;
+      let height = element.scrollHeight + 5;
       element.style.cssText = 'height:' + height + 'px';
-    },0)
+    }, 0)
+  }
+
+  public uploadImage(event) {
+    if(event.target.files[0]){  //take care of the event time 
+      this.file = event.target.files[0];
+      console.log("this.file", this.file);
+      var reader = new FileReader();
+      reader.onload = () => {
+        this.imgPreview = reader.result;
+      };
+      reader.readAsDataURL(this.file);
+      console.log(reader, "reader")
+    } else {
+      console.log("Please select an image");
+    }    
+  }
+
+  public triggerFileBtn() {
+    document.getElementById("valueInput").click();
   }
 
 }

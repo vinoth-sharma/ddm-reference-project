cf387d910292213a3b23be1899c3641ad749761e
bz1ksc <aneesha.biju@gm.com>
2019-05-01 19:40:56 +0530

    chips for input emails

--- src/app/share-reports/share-reports.component.ts
+++ src/app/share-reports/share-reports.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-// import  { froalaEditor} from froala
-// import * as froalaEditor from 'froala-editor/js/froala_editor.pkgd.min.js';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatChipInputEvent } from '@angular/material';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { FormControl, Validators } from '@angular/forms';
+import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material';
+import Utils from "../../utils";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: 'app-share-reports',
@@ -9,29 +13,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShareReportsComponent implements OnInit {
 
+  @Input() selectedId: number;
+  @Input() selectedName: string;
   public shareData: any = {};
   public formats: any = [];
   public deliveryMethods: any = [];
   public sheetList: any = [];
   public isSheets: boolean;
   public isSignature: boolean;
-  public imagePath;
   imgUrl: any;
   imgPreview: any;
   file: File;
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  emails = [];
 
-  constructor() { }
+
+  constructor(private toasterService: ToastrService) { }
 
   ngOnInit() {
     this.initialState();
-    // $(function() {
-    //   $('textarea#froala-editor').froalaEditor()
-    // });
   }
 
-  /**
-   * initialState
-   */
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.emails.push({ name: value.trim() });
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(email): void {
+    const index = this.emails.indexOf(email);
+    if (index >= 0) {
+      this.emails.splice(index, 1);
+    }
+  }
+
+
   public initialState() {
     this.shareData = {
       'reportName': '',
@@ -63,16 +92,12 @@ export class ShareReportsComponent implements OnInit {
     this.isSignature = false;
   };
 
-  /**
-   * shareReport
-   */
-  public shareReport() {
 
+  public shareReport() {
+    // when will it be properly shared ? toastermsg ?
   };
 
-  /**
-   * autoSize
-   */
+
   public autoSize(el) {
     let element = el;
     setTimeout(function () {
@@ -83,18 +108,19 @@ export class ShareReportsComponent implements OnInit {
   }
 
   public uploadImage(event) {
-    if(event.target.files[0]){  //take care of the event time 
+    if (event.target.files[0]) {  //take care of the loading time 
       this.file = event.target.files[0];
       console.log("this.file", this.file);
       var reader = new FileReader();
       reader.onload = () => {
         this.imgPreview = reader.result;
+        this.imgUrl = true;
       };
       reader.readAsDataURL(this.file);
       console.log(reader, "reader")
     } else {
       console.log("Please select an image");
-    }    
+    }
   }
 
   public triggerFileBtn() {

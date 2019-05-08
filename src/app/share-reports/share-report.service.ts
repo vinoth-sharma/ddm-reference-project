435876fe6e2052bbf5372a3e07ff09be19818f93
bz1ksc <aneesha.biju@gm.com>
2019-05-08 15:43:28 +0530

    get signature and API integrations

--- src/app/share-reports/share-report.service.ts
+++ src/app/share-reports/share-report.service.ts
@@ -26,47 +26,43 @@ export class ShareReportService {
   }
 
   public createSign(options) {
-    let serviceUrl = `${environment.baseUrl}reports/reports_signature/`;
-    let requestBody = {
-      user_id: options.user_id,
-      report_list_id: options.report_list_id
-    };
-    return this.http
-      .post(serviceUrl, requestBody)
-      .pipe(catchError(this.handleError));
+    // let serviceUrl = `${environment.baseUrl}reports/reports_signature/`;
+    // let requestBody = {
+    //   user_id: options.user_id,
+    //   report_list_id: options.report_list_id
+    // };
+    // return this.http
+    //   .post(serviceUrl, requestBody)
+    //   .pipe(catchError(this.handleError));
   }
 
   public putSign(options) {
-    let serviceUrl = `${environment.baseUrl}reports/reports_signature/`;
-    let requestBody = {
-      user_id: options.user_id,
-      report_list_id: options.report_list_id
-    };
-    return this.http
-      .put(serviceUrl, requestBody)
-      .pipe(catchError(this.handleError));
+    // let serviceUrl = `${environment.baseUrl}reports/reports_signature/`;
+    // let requestBody = {
+    //   user_id: options.user_id,
+    //   report_list_id: options.report_list_id
+    // };
+    // return this.http
+    //   .put(serviceUrl, requestBody)
+    //   .pipe(catchError(this.handleError));
   }
 
   public shareToUsers(options) {
     let serviceUrl = `${environment.baseUrl}reports/sharing_report/`;
-    let requestBody = {
-      report_name: options.user_id,
-      report_list_id: options.report_list_id,
-      file_format: options.file_format,
-      delivery_method: options.delivery_method,
-      recepeint_list: options.user_id,
-      file_upload: options.user_id,
-      description: options.user_id,
-      signature_html: options.user_id
-    };
+    let requestBody = new FormData();
+    requestBody.append('report_name', options.user_id);
+    requestBody.append('report_list_id', options.report_list_id);
+    requestBody.append('file_format', options.file_format);
+    requestBody.append('delivery_method', options.delivery_method);
+    requestBody.append('recepeint_list', options.recepeint_list);
+    requestBody.append('description', options.description);
+    // requestBody.append('signature_html', options.user_id);
+    // requestBody.append('file_upload', fileObject);
+    //   options['description'] = this.description;
     return this.http
       .post(serviceUrl, requestBody)
       .pipe(catchError(this.handleError));
   }
-
-  // pdfUpload() {
-  //   this.http.post('', );
-  // }
 
   uploadFile(fileObject: File) {
     let serviceUrl = `${environment.baseUrl}reports/image_upload/`;
--- src/app/share-reports/share-reports.component.ts
+++ src/app/share-reports/share-reports.component.ts
@@ -6,7 +6,7 @@ import { ShareReportService } from './share-report.service'
 import Utils from "../../utils";
 import { ToastrService } from "ngx-toastr";
 import ClassicEditor from '../../assets/cdn/ckeditor/ckeditor.js';
-import { Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthenticationService } from "../authentication.service";
 declare var $: any;
 
@@ -54,6 +54,8 @@ export class ShareReportsComponent implements OnInit {
   fileName;
   defaultError = "There seems to be an error. Please try again later.";
   @ViewChild('#editSign') editSign: ElementRef;
+  public fileUpload: boolean = false;
+  maxSignId: number;
 
   constructor(private route: Router,
     private toasterService: ToastrService,
@@ -75,38 +77,17 @@ export class ShareReportsComponent implements OnInit {
     console.log(this.fruitCtrl.value, "input")
     const value = event.value;
     console.log(value, "value")
-    this.getPatternError();
     this.getDuplicateMessage();
-    if ((value || '').trim() && this.isValid && !this.isDuplicate) {
+    if ((value || '').trim() && !this.fruitCtrl.invalid && !this.isDuplicate ) {
       this.emails.push(value.trim());
       //   this.isDuplicate = false;
       // this.isValid = true;
+    } else {
+      console.log('Error');
     }
     this.fruitCtrl.setValue('');
   }
 
-  getPatternError() {
-    console.log('Checking value', this.fruitCtrl.value);
-    let email = this.fruitCtrl.value;
-    if (email.includes('@')) {
-      if (email.endsWith("@gm.com")) {
-        this.isValid = true;
-      } else {
-        this.isValid = false;
-        console.log("not gm");
-      }
-    }
-    else {
-      this.isValid = true;
-    }
-    // if(this.fruitCtrl.errors?.pattern) {
-    //   this.isValid = false;
-    //   console.log("valid");
-    // } else {
-    //   this.isValid = true;
-    //   console.log("invalid");
-    // }
-  };
 
   getDuplicateMessage() {
     if (this.emails.includes(this.fruitCtrl.value)) {
@@ -128,6 +109,7 @@ export class ShareReportsComponent implements OnInit {
   reset() {
     console.log(this.pdfFile.nativeElement.files);
     this.pdfFile.nativeElement.value = "";
+    this.fileUpload = false;
     console.log(this.pdfFile.nativeElement.files);
   }
 
@@ -177,6 +159,9 @@ export class ShareReportsComponent implements OnInit {
 
   uploadPdf(event) {
     this.file = event.target.files[0];
+    if (this.file) {
+      this.fileUpload = true;
+    }
     console.log("file", this.file);
     this.fileName = this.file.name;
     console.log("name", this.fileName);
@@ -197,59 +182,69 @@ export class ShareReportsComponent implements OnInit {
   }
 
   public fetchSignatures() {
-    this.shareReportService.getSignatures().subscribe(res => {
-      this.signatures = [{ "signature_id": -9999,
+    this.shareReportService.getSignatures().subscribe((res: {
+      data: {
+        signature_id: number,
+        signature_name: string,
+        signature_html: string,
+        user_id: string,
+        image_id: number
+      }[]
+    }) => {
+      this.maxSignId = Math.max.apply(null, res.data.map(sign => sign.signature_id)) + 1;
+    this.signatures = [{
+      "signature_id": this.maxSignId,
       "signature_name": "Create new signature",
       "signature_html": "",
-      "user_id": -9999,
-      "image_id": null }, ...res['data']];
-      this.selectSign = this.signatures[0].signature_name;
-      console.log("this.signatures ", this.signatures)
+      "user_id": 'USER1',
+      "image_id": null
+    }, ...res['data']];
+    this.selectSign = this.signatures[0].signature_name;
+    console.log("this.signatures ", this.signatures)
+  })
+}
+
+select(event) {
+  this.signSelected = true;
+  // this.selectSign = event.target.value;
+  console.log(this.selectSign, "name");
+  const selectedSign = this.signatures.find(x =>
+    x.signature_name.trim().toLowerCase() == this.selectSign.trim().toLowerCase());
+  this.editorData = selectedSign.signature_html;
+  if (selectedSign.user_id === -9999) {
+    $('#signature').modal('show');
+  }
+  console.log(this.editorData, "html");
+}
+
+CreateSignature() {
+  // check if the name is unique
+}
+
+updateSignatureData() {
+  // in the pop-up
+}
+
+updateSharingData() {
+  let options = {};
+  Utils.showSpinner();
+  options['report_name'] = this.selectedId;
+  options['report_list_id'] = this.selectedName;
+  options['file_format'] = "xlsx";
+  options['delivery_method'] = "Email";
+  options['recepeint_list'] = this.emails;
+  options['file_upload'] = this.file;
+  options['description'] = this.description;
+  // if new or old
+  options['signature_html'] = this.editorData    // which editordata
+  this.shareReportService.shareToUsers(options).subscribe(
+    res => {
+      this.toasterService.success("Report has been shared successfully")
+      Utils.hideSpinner();
+      Utils.closeModals();
     })
+  err => {
+    this.toasterService.error(err.message || this.defaultError);
   }
-
-  select(event) {
-    this.signSelected = true;
-    this.selectSign = event.target.value;
-    console.log(this.selectSign, "name");
-    const selectedSign = this.signatures.find(x =>
-      x.signature_name.trim().toLowerCase() == this.selectSign.trim().toLowerCase());
-      this.editorData = selectedSign.signature_html;
-    if (selectedSign.user_id === -9999) {
-      $('#signature').modal('show');
-    }
-    console.log(this.editorData, "html");
-  }
-
-  CreateSignature() {
-    // check if the name is unique
-  }
-
-  updateSignatureData() {
-    // in the pop-up
-  }
-
-  updateSharingData() {
-    let options = {};
-    Utils.showSpinner();
-    options['report_name'] = this.selectedId;
-    options['report_list_id'] = this.selectedName;
-    options['file_format'] = "xlsx";
-    options['delivery_method'] = "Email";
-    options['recepeint_list'] = this.emails;
-    // options['file_upload'] = options.user_id,
-    options['upload_filename'] = this.fileName,
-      options['description'] = this.description;
-    // if new or old
-    // options['signature_html'] : options.user_id    // which editordata
-    this.shareReportService.shareToUsers(options).subscribe(
-      res => {
-        this.toasterService.success("Report has been shared successfully")
-        Utils.hideSpinner();
-        Utils.closeModals();
-      })
-    err => {
-      this.toasterService.error(err.message || this.defaultError);
-    }
-  };
+};
 }

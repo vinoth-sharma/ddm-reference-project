c6b4112be7e38511eae3b5bb75ea8c07e14c5ed6
Deepak Urs <deepak.urs@mu-sigma.com>
2019-05-11 15:21:15 +0530

    clean scheduled reports component

--- src/app/scheduled-reports/scheduled-reports.component.ts
+++ src/app/scheduled-reports/scheduled-reports.component.ts
@@ -1,6 +1,6 @@
+import {MatPaginator, MatTableDataSource, MatSort} from '@angular/material';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
-import { MatSort, MatTableDataSource } from '@angular/material';
 import { ToastrService } from "ngx-toastr";
 import { Router } from '@angular/router';
 
@@ -14,8 +14,9 @@ declare var $: any;
   styleUrls: ['./scheduled-reports.component.css']
 })
 
-export class ScheduledReportsComponent implements OnInit {
+export class ScheduledReportsComponent {
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   public dataSource: any;
@@ -31,69 +32,73 @@ export class ScheduledReportsComponent implements OnInit {
   public scheduleDataToBeSent:any = {};
   public isLoading: boolean;
   public semanticLayerId: number;
-  // public scheduleReportIdFlag: number;
 
   constructor(private scheduleService:ScheduleService,
-              private toasterService: ToastrService,
-              public router: Router,
-              public authenticationService: AuthenticationService) { }
+    private toasterService: ToastrService,
+    public router: Router,
+    public authenticationService: AuthenticationService) { }
 
-  ngOnInit() {
+    ngOnInit() {
+      this.getSemanticId();
+      this.tableSorting();
+      // this.isEmptyTables = false;
+      // this.scheduleData.created_by = this.authenticationService.userId;
+    }
+
+  // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+
+
+  public tableSorting(){
+  this.scheduleService.getScheduledReports(this.semanticLayerId).subscribe(res =>{
+    this.dataSource = res['data']
+    // console.log("SCHEDULED REPORTS LIST BEFORE",this.dataSource);
+    
+    // filtering the result
+    //transforming export_format
+    this.dataSource.map( temp => { 
+      if( temp["export_format"] == 1){ temp["export_format"] = "CSV" } 
+      else  if( temp["export_format"] == 2){ temp["export_format"] = "Excel" }  
+      else if( temp["export_format"] == 3){ temp["export_format"] = "Pdf"}  
+      else if( temp["export_format"] == 4){ temp["export_format"] = "Text" }  
+      else if( temp["export_format"] == 5){ temp["export_format"] = "HTML" }  
+      else if( temp["export_format"] == 6){ temp["export_format"] = "XML" }  
+    });
+
+    //transforming sharing_mode
+    this.dataSource.map( temp => { 
+      if( temp["sharing_mode"] == 1){ temp["sharing_mode"] = "Email" } 
+      else if( temp["sharing_mode"] == 2){ temp["sharing_mode"] = "Shared Drive" }  
+      else if( temp["sharing_mode"] == 3){ temp["sharing_mode"] = "FTP"} 
+      else { temp["sharing_mode"] = "Unknown format"} 
+    });
+
+    if (typeof (this.dataSource) == 'undefined' || this.dataSource.length == 0) {  
+      this.isEmptyTables = true;
+    }
+
     this.getSemanticId();
-    this.tableSorting();
-    this.isEmptyTables = false;
-    // this.scheduleData.created_by = this.authenticationService.userId;
+    this.dataSource = new MatTableDataSource(this.dataSource);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.isLoading = false;
+  }, error => {
+    this.toasterService.error(this.defaultError);
+    this.isLoading = false;
+  });
+  // ngOnInit() {
+    
+  // }
   }
 
-  // public sharingModes = [
-  //   {'value': 1, 'display': 'Email'},
-  //   {'value': 2, 'display': 'Shared Drive'},
-  //   {'value': 3, 'display': 'FTP'},
-  // ]
 
-  public tableSorting() {
-    this.isLoading = true;
-    // for loading data to display(whole)
-    this.scheduleService.getScheduledReports(this.semanticLayerId).subscribe(res =>{
-      this.dataSource = res['data']
-      // console.log("SCHEDULED REPORTS LIST BEFORE",this.dataSource);
-      
-      // filtering the result
-      //transforming export_format
-      this.dataSource.map( temp => { 
-        if( temp["export_format"] == 1){ temp["export_format"] = "CSV" } 
-        else  if( temp["export_format"] == 2){ temp["export_format"] = "Excel" }  
-        else if( temp["export_format"] == 3){ temp["export_format"] = "Pdf"}  
-        else if( temp["export_format"] == 4){ temp["export_format"] = "Text" }  
-        else if( temp["export_format"] == 5){ temp["export_format"] = "HTML" }  
-        else if( temp["export_format"] == 6){ temp["export_format"] = "XML" }  
-        // temp["export_format"]  =  this.sharingModes[temp["export_format"]].display;
-      });
-
-      //transforming sharing_mode
-      this.dataSource.map( temp => { 
-        if( temp["sharing_mode"] == 1){ temp["sharing_mode"] = "Email" } 
-        else if( temp["sharing_mode"] == 2){ temp["sharing_mode"] = "Shared Drive" }  
-        else if( temp["sharing_mode"] == 3){ temp["sharing_mode"] = "FTP"} 
-        else { temp["sharing_mode"] = "Unknown format"} 
-      });
-
-      // console.log("modified SCHEDULED REPORTS LIST",this.dataSource);
-
-      if (typeof (this.dataSource) == 'undefined' || this.dataSource.length == 0) {  
-        this.isEmptyTables = true;
+  public getSemanticId() {
+    this.router.config.forEach(element => {
+      if (element.path == "semantic") {
+        this.semanticLayerId = element.data["semantic_id"];
+        // console.log("PROCURED SL_ID",this.semanticLayerId);
       }
-
-
-      this.dataSource = new MatTableDataSource(this.dataSource);
-      this.dataSource.sort = this.sort;
-      this.isLoading = false;
-    }, error => {
-      this.toasterService.error(this.defaultError);
-      this.isLoading = false;
     });
-  };
-
+  }
 
   public goToReports(reportName:string){
     Utils.showSpinner();
@@ -114,13 +119,8 @@ export class ScheduledReportsComponent implements OnInit {
     });
 
   }
-
-  public getSemanticId() {
-    this.router.config.forEach(element => {
-      if (element.path == "semantic") {
-        this.semanticLayerId = element.data["semantic_id"];
-        // console.log("PROCURED SL_ID",this.semanticLayerId);
-      }
-    });
-  }
 }
+
+
+
+

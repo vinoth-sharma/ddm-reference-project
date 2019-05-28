bcfd3aaeb6f5799471a463d563fea9aca2d1f81e
Nithya <nithya.murali@mu-sigma.com>
2019-05-28 14:36:06 +0530

    updated zip path for report download

--- src/app/report-manipulation/report/insert/insert.component.ts
+++ src/app/report-manipulation/report/insert/insert.component.ts
@@ -375,22 +375,14 @@ export class InsertComponent implements OnInit {
   }
 
   exportReport(format: any) {     
-    // let data = {
-    //   report_list_id: this.reportId,
-    //   file_type: format.type
-    // };
-
     let data = {
-      report_list_id: [this.reportId],
-      action: 'download',
+      report_list_id: this.reportId,
       file_type: format.type
     };
 
-
     this.isDownloading = true;
     this.reportsService.exportReport(data).subscribe(response => {
-      // this.createDownloadLink(response['all_file_path']['zip_url']);
-      this.createDownloadLink(response['wb_path'][0]);
+      this.createDownloadLink(response['all_file_path']['zip_url']);
     }, error => {
       this.showToastMessage(error['message'].error || this.defaultError, 'error');
       this.isDownloading = false;

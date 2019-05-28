e1abb2450a1c728245f15a8eaabd24e307dbd9f3
bz1ksc <aneesha.biju@gm.com>
2019-05-28 16:45:56 +0530

    validation for conditions and log entry revamp

--- src/app/log-entry/log-entry.component.ts
+++ src/app/log-entry/log-entry.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { SecurityModalService } from '../security-modal/security-modal.service';
 import { ToastrService } from "ngx-toastr";
-import { MatTableDataSource, MatPaginator } from '@angular/material';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 import { Router } from "@angular/router"
 
 @Component({
@@ -12,6 +12,7 @@ import { Router } from "@angular/router"
 export class LogEntryComponent implements OnInit {
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   defaultError = "There seems to be an error. Please try again later.";
   logData = {};
@@ -20,15 +21,17 @@ export class LogEntryComponent implements OnInit {
   public reportsDataSource: any;
   public tablesDataSource: any;
   isEmpty: boolean;
-  isLoadingRoles: boolean = true;
-  isLoadingReports: boolean = true;
-  isLoadingTables: boolean = true;
-  isLoadingSemantic: boolean = true;
-  public rolesColumns = ['changed_by_user_name', 'changed_for_user_name', 'change_type', 'change_description', 'change_timestamp', 'sl_name'];
-  public semanticColumns = ['sl_name', 'change_type', 'change_description', 'change_timestamp'];
-  public reportsColumns = ['report_name', 'sl_name', 'change_type', 'change_description', 'change_timestamp'];
-  public tablesColumns = ['sl_name', 'change_type', 'change_description', 'new_name', 'change_timestamp'];
-
+  isLoading: boolean = true;
+  chosen : string = 'Roles and Responsibilities';
+  public rolesColumns: string[] = ['changed_by_user_id','changed_by_user_name','changed_for_user_id', 'changed_for_user_name', 'change_type', 'change_description', 'change_timestamp', 'sl_name'];
+  public semanticColumns: string[] = ['changed_by_user_id','changed_by_user_name','changed_for_user_id', 'changed_for_user_name','sl_name', 'change_type', 'change_description', 'change_timestamp'];
+  public reportsColumns: string[]  = ['report_name', 'sl_name', 'change_type', 'change_description', 'change_timestamp'];
+  public tablesColumns: string[]  = ['sl_name', 'change_type', 'change_description', 'new_name', 'change_timestamp'];
+  public selections = ['Roles and Responsibilities','Semantic Layer','Tables and Custom tables','Reports'];
+  selected: string = 'Roles and Responsibilities';
+  // applyFilter(filterValue : string) {
+  //       this.rolesDataSource.filter = filterValue.trim().toLowerCase();
+  // }
   constructor(private semanticModalService: SecurityModalService,
     private toasterService: ToastrService,
     private router: Router) { }
@@ -41,10 +44,12 @@ export class LogEntryComponent implements OnInit {
     // this.tablesDataSource.paginator = this.paginator;
     this.semanticModalService.getLogData(1).subscribe(res => {
       this.logData = res;
-      this.isLoadingRoles = false;
+      this.isLoading = false;
       this.rolesDataSource = this.logData['data'];
       this.rolesDataSource = new MatTableDataSource(this.rolesDataSource);
-      this.rolesDataSource.paginator = this.paginator; })
+           this.rolesDataSource.paginator = this.paginator;
+      this.rolesDataSource.sort = this.sort; 
+    })
     //   }, error => {
     //     this.toasterService.error(this.defaultError);
 
@@ -57,12 +62,13 @@ export class LogEntryComponent implements OnInit {
     this.router.navigate(['semantic/sem-sl/sem-existing']);
   }
 
-
-  tabChanged(event) {
-    if (event.tab.textLabel == "Semantic Layer") {
+  onSelect(event) {
+    this.isLoading= true;
+    if (event.source.value == "Semantic Layer") {     
+     this.chosen = 'Semantic Layer';
       this.semanticModalService.getLogData(2).subscribe(res => {
         this.logData = res;
-        this.isLoadingSemantic = false;
+        this.isLoading = false;
         this.semanticDataSource = this.logData['data'];
         this.semanticDataSource = new MatTableDataSource(this.semanticDataSource);
         this.semanticDataSource.paginator = this.paginator;
@@ -70,10 +76,11 @@ export class LogEntryComponent implements OnInit {
           this.isEmpty = true;
         }
       })
-    } else if (event.tab.textLabel == "Reports") {
+    } else if (event.source.value == "Reports") {
+      this.chosen = 'Reports';
       this.semanticModalService.getLogData(4).subscribe(res => {
         this.logData = res;
-        this.isLoadingReports = false;
+        this.isLoading = false;
         this.reportsDataSource = this.logData['data'];
         this.reportsDataSource = new MatTableDataSource(this.reportsDataSource);
         this.reportsDataSource.paginator = this.paginator;
@@ -81,21 +88,24 @@ export class LogEntryComponent implements OnInit {
           this.isEmpty = true;
         }
       })
-    } else if (event.tab.textLabel == "Roles and Responsibilities") {
+    } else if (event.source.value == "Roles and Responsibilities") {
+      this.chosen = 'Roles and Responsibilities';
       this.semanticModalService.getLogData(1).subscribe(res => {
         this.logData = res;
-        this.isLoadingRoles = false;
+        this.isLoading = false;
         this.rolesDataSource = this.logData['data'];
         this.rolesDataSource = new MatTableDataSource(this.rolesDataSource);
         this.rolesDataSource.paginator = this.paginator;
+        this.rolesDataSource.sort = this.sort;
         if (typeof (this.rolesDataSource) == 'undefined' || this.rolesDataSource.length == 0) {
           this.isEmpty = true;
         }
       })
-    } else if (event.tab.textLabel == "Tables and Custom tables") {
+    } else if (event.source.value == "Tables and Custom tables") {
+      this.chosen = 'Tables and Custom tables';
       this.semanticModalService.getLogData(3).subscribe(res => {
         this.logData = res;
-        this.isLoadingTables = false;
+        this.isLoading = false;
         this.tablesDataSource = this.logData['data'];
         this.tablesDataSource = new MatTableDataSource(this.tablesDataSource);
         this.tablesDataSource.paginator = this.paginator;

4ba6071d8da85fc21510de05c521a72c6a6a0440
Nithya <nithya.murali@mu-sigma.com>
2019-05-31 18:33:00 +0530

    added aggregations for pivots

--- src/app/report-manipulation/report/pivot-builder/pivot-builder.component.ts
+++ src/app/report-manipulation/report/pivot-builder/pivot-builder.component.ts
@@ -8,6 +8,7 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
   templateUrl: './pivot-builder.component.html',
   styleUrls: ['./pivot-builder.component.scss']
 })
+
 export class PivotBuilderComponent implements OnInit {
   @Input() view;
   public columns = [];
@@ -16,12 +17,22 @@ export class PivotBuilderComponent implements OnInit {
   public selectedRows = [];
   public selectedColumns = [];
   public selectedValues = [];
+
+  public aggregations = ['Sum', 'Average', 'Max', 'Min'];
+  public selectedAggregations = [];
+
+  // public edit: boolean;
+  public edit = {};
+
   @Input() public pivotData = {
     data: this.tableData,
     rows: this.selectedRows,
     columns: this.selectedColumns,
     values: this.selectedValues,
     filters: this.selectedFilters,
+
+    aggregations: this.selectedAggregations,
+
     _data: []
   };
   @Output() update = new EventEmitter();
@@ -40,8 +51,14 @@ export class PivotBuilderComponent implements OnInit {
       this.selectedColumns = this.pivotData.columns;
       this.selectedRows = this.pivotData.rows;
       this.selectedValues = this.pivotData.values;
+
+      this.selectedAggregations = this.pivotData.aggregations;
+
       this.tableData = this.pivotData.data;
     }
+
+    console.log('pivotData', this.pivotData);
+    
   }
 
   ngOnInit() {
@@ -51,21 +68,35 @@ export class PivotBuilderComponent implements OnInit {
       this.selectedColumns = this.pivotData.columns;
       this.selectedRows = this.pivotData.rows;
       this.selectedValues = this.pivotData.values;
+
+      this.selectedAggregations = this.pivotData.aggregations;
+
       this.tableData = this.pivotData.data;
       this.columns = Object.keys(this.data[0]);
+
+      console.log('ngoninit', this.pivotData, this.selectedValues);
+      
     }
   }
 
   updateTableData() {
-    this.reportsService.getAggregatedTable(this.data, this.selectedRows, this.selectedValues)
+    // this.reportsService.getAggregatedTable(this.data, this.selectedRows, this.selectedValues)
+    this.reportsService.getAggregatedTable(this.data, this.selectedRows, this.selectedAggregations)
+
       .then((res: any[]) => {
         this.tableData = res;
         this.pivotData = {
           data: this.tableData,
           rows: this.selectedRows,
           columns: this.selectedColumns,
+          
+          // values: this.selectedAggregations,
+
           values: this.selectedValues,
           filters: this.selectedFilters,
+
+          aggregations: this.selectedAggregations,
+
           _data: this.data
         };
         this.update.emit(this.pivotData);
@@ -85,5 +116,45 @@ export class PivotBuilderComponent implements OnInit {
     this.selectedRows = this.selectedRows.filter(item => item !== row);
     this.updateTableData();
   }
+
+  removeValue(value: string) {
+    this.selectedValues = this.selectedValues.filter(item => item !== value);
+    
+    this.selectedAggregations.splice(this.selectedValues.indexOf(value), 1);
+
+    // this.aggregation[value] = '';
+
+    console.log('removeValue', value, this.selectedValues.indexOf(value), this.selectedValues, this.selectedAggregations);   
+
+    this.updateTableData();
+  }
+
+  public aggregation = {};
+
+  setAggregation() {
+
+    // this.edit[index] = false;
+
+    for (let i = 0; i < this.selectedValues.length; i++) {
+      
+      let item = {
+        field: this.selectedValues[i],
+        aggregation: this.aggregation[this.selectedValues[i]]
+      }
+      
+      // this.selectedAggregations[i] = item;
+
+      this.selectedAggregations[this.selectedValues[i]] = item;
+
+    }
+
+    console.log('setAggregation', this.selectedValues, this.selectedAggregations, this.aggregation);
+
+    this.updateTableData();
+  }
+
+  // toggleEdit(index: number) {
+  //   this.edit[index] = !this.edit[index];
+  // }
 
 }

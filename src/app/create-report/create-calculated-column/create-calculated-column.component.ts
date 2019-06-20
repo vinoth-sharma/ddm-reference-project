3828a014d26ccc7a80c1c3c38b9dd7b300c0901c
Sahana P <sonal.raut@gm.com>
2019-06-20 16:07:41 +0530

    1500764, 1500782, 1450263, 1453664, space and default issue in parameter issues fixed

--- src/app/create-report/create-calculated-column/create-calculated-column.component.ts
+++ src/app/create-report/create-calculated-column/create-calculated-column.component.ts
@@ -381,7 +381,19 @@ return true;
           }
         });
     
-        if (currentList.length > 0 || existingList.length > 0) {
+        let tableList = this.tables.filter(element => {
+          if(type === 'column'){
+            return value.toLowerCase() === element['name'].toLowerCase();
+          }
+        });
+
+        let columnList = this.columns.filter(element => {
+          if(type === 'column'){
+            return value.toLowerCase() === element.split('.')[1].toLowerCase();
+          }
+        });
+
+        if (currentList.length > 0 || existingList.length > 0 || tableList.length > 0 || columnList.length > 0) {
           type === 'column'?this.columnName.setErrors({'incorrect': false}):this.queryTextarea.setErrors({'incorrect': false});
         } else {
           type === 'column'?this.columnName.setErrors(null):this.queryTextarea.setErrors(null);

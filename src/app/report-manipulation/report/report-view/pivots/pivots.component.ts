338adae6d13030646ec042f7808621d1ee87a9e5
Nithya <nithya.murali@mu-sigma.com>
2019-07-22 19:10:44 +0530

    made similar kind of selection UX for charts/pivots/clone

--- src/app/report-manipulation/report/report-view/pivots/pivots.component.ts
+++ src/app/report-manipulation/report/report-view/pivots/pivots.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { FormControl } from '@angular/forms';
+import { OnInit, Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-pivots',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PivotsComponent implements OnInit {
 
-  constructor() { }
+  toppings = new FormControl();
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato', 'Pepperoni', 'Sausage', 'Tomato'];
+  
+  constructor(public dialogRef: MatDialogRef<PivotsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data:any) { }
 
   ngOnInit() {
   }
-
+ 
+  closeDailog():void{
+    this.dialogRef.close();
+  }
 }

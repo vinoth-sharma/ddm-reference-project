0ed048553f558ccf4c19b9c27fa1d5f6fc88f31f
Nithya <nithya.murali@mu-sigma.com>
2019-07-23 19:36:03 +0530

    added apis

---
+++ src/app/report-manipulation/report/report-view/report.apis.ts
@@ -0,0 +1,3 @@
+const report_creation = `http://127.0.0.1:8000/reports/report_creation`
+const get_report_list = `http://127.0.0.1:8000/reports/get_report_list?`
+const get_report_sheet_data = `http://127.0.0.1:8000/reports/get_report_sheet_data`

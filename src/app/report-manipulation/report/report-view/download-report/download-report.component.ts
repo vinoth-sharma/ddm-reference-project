a0d3c2599bdf1b384fa4d9995136bcb4312f6ca4
Nithya <nithya.murali@mu-sigma.com>
2019-07-31 15:43:22 +0530

    download report is done

--- src/app/report-manipulation/report/report-view/download-report/download-report.component.ts
+++ src/app/report-manipulation/report/report-view/download-report/download-report.component.ts
@@ -14,51 +14,51 @@ export class DownloadReportComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<DownloadReportComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     public reportService: ReportViewService) { }
-    sheets = new FormControl()
-    disableMultiSelect :boolean = false;
-    enableSubmitBtn :boolean = false;
+  sheets = new FormControl()
+  disableMultiSelect: boolean = false;
+  enableSubmitBtn: boolean = false;
   sheetData: any = [];
   selectedSheets = [];
   selectedFormat = 'xlsx';
 
   ngOnInit() {
-
-    console.log(this.data);
+    // console.log(this.data);
     this.sheetData = this.reportService.getSheetsFromReport();
-    console.log(this.sheetData);
-
+    // console.log(this.sheetData);
   }
-  
-  formValidate(){
-    if(this.disableMultiSelect){
-      console.log('if');
-      
-      this.enableSubmitBtn = this.selectedFormat?true:false;
+
+  formValidate() {
+    if (this.disableMultiSelect) {
+      this.enableSubmitBtn = this.selectedFormat ? true : false;
     }
-    else{
-      this.enableSubmitBtn = this.selectedFormat && this.selectedSheets.length > 0?true:false;
+    else {
+      this.enableSubmitBtn = this.selectedFormat && this.selectedSheets.length > 0 ? true : false;
     }
-    console.log(this.enableSubmitBtn);
-    
   }
 
   sheetSelected(event) {
-    console.log(event);
+    // console.log(event);
     this.selectedSheets = event.value;
     this.formValidate();
   }
 
-  selectAllSheet(event){
-    console.log(event);
+  selectAllSheet(event) {
+    // console.log(event);
     this.disableMultiSelect = event.checked;
     this.formValidate();
   }
-  
-  downloadReport(){
-    let sheets = this.disableMultiSelect?[]:this.selectedSheets;
-    this.reportService.downloadReportFile(sheets,this.selectedFormat).subscribe(res=>{
-      console.log(res);
-      
+
+  downloadReport() {
+    let sheets = this.disableMultiSelect ? [] : this.selectedSheets;
+    this.reportService.downloadReportFile(sheets, this.selectedFormat).subscribe((res: any) => {
+      // console.log(res);
+      const url = window.URL.createObjectURL(res.data)
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', res.fileName)
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
     })
   }
 
--- src/app/report-manipulation/report/report-view/report-view.service.ts
+++ src/app/report-manipulation/report/report-view/report-view.service.ts
@@ -4,7 +4,7 @@ import { environment } from 'src/environments/environment';
 import { Observable, of, Subject } from 'rxjs';
 import { switchMap, map, catchError } from 'rxjs/operators';
 import { GlobalReportServices } from "./global.reports.service";
-import { get_report_sheet_data, report_creation, uploadFile, deleteReportOrSheet, renameSheet, downloadReportFile } from "./report.apis";
+import { get_report_sheet_data, report_creation, uploadFile, deleteReportOrSheet, renameSheet, downloadReportFileApi } from "./report.apis";
 import { element } from '@angular/core/src/render3/instructions';
 
 @Injectable({
@@ -278,9 +278,16 @@ export class ReportViewService {
     if(sheets.length > 0)
       obj['sheet_ids'] = sheets
 
-    return this._http.post(downloadReportFile,obj).pipe(
-      map(res => {
+    return this._http.post(downloadReportFileApi,obj,{
+      responseType: 'blob' , observe :'response'
+    }).pipe(
+      map((res:any) => {
         console.log(res);
+        console.log(res.headers());
+        let cd = res.headers.get('Content-Type')
+        // console.log(cd);
+        
+        return { data:res.body , fileName: this.globalService.getSelectedIds().report_id+'.zip'}
       }),
       catchError(this.handleError)
     )

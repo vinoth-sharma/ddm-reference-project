e8ee225b3303bbc9f2a303c38ff332d525f2f40b
Deepak Urs <deepak.urs@mu-sigma.com>
2019-08-08 20:56:34 +0530

    cemasntic layer dropdowns space optimization

--- src/app/semantic-new/semantic-new.component.ts
+++ src/app/semantic-new/semantic-new.component.ts
@@ -50,7 +50,7 @@ export class SemanticNewComponent {
     textField: 'mapped_table_name',
     idField: 'sl_tables_id',
     selectAllText: 'Select All',
-    itemsShowLimit: 3,
+    itemsShowLimit: 8,
     allowSearchFilter: true,
     enableCheckAll: true
   };
@@ -61,7 +61,7 @@ export class SemanticNewComponent {
     textField: 'table_name',
     selectAllText: 'Select All',
     unSelectAllText: 'UnSelect All',
-    itemsShowLimit: 3,
+    itemsShowLimit: 8,
     allowSearchFilter: true,
     enableCheckAll: true,
     maxHeight:200
@@ -73,10 +73,10 @@ export class SemanticNewComponent {
     textField: 'mapped_table_name',
     selectAllText: 'Select All',
     unSelectAllText: 'UnSelect All',
-    itemsShowLimit: 3,
+    itemsShowLimit: 8,
     allowSearchFilter: true,
     enableCheckAll: true,
-    maxHeight:142
+    maxHeight:200
   };
 
   constructor(

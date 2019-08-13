99ede2c37f6ddb08c34deae6a64aca87181bf298
Nithya <nithya.murali@mu-sigma.com>
2019-08-13 21:16:48 +0530

    deployment commit

--- src/app/report-manipulation/report/report-view/report.apis.ts
+++ src/app/report-manipulation/report/report-view/report.apis.ts
@@ -9,8 +9,8 @@ export const get_report_list = `${environment.baseUrl}reports/get_report_list/`
 export const get_report_sheet_data = `${environment.baseUrl}reports/get_report_sheet_data`
 // export const get_report_sheet_data = `https://ddm1.apps.pcfepg2wi.gm.com/reports/get_report_sheet_data`
 //method - POST
-// export const uploadFile = `${environment.baseUrl}reports/upload_sheet_to_ecs/`
-export const uploadFile = `https://ddm1.apps.pcfepg2wi.gm.com/reports/upload_sheet_to_ecs/`
+export const uploadFile = `${environment.baseUrl}reports/upload_sheet_to_ecs/`
+// export const uploadFile = `https://ddm1.apps.pcfepg2wi.gm.com/reports/upload_sheet_to_ecs/`
 //method - DELETE
 export const deleteReportOrSheet = `${environment.baseUrl}reports/delete_report_or_sheet/`
 //method - PUT
@@ -27,5 +27,5 @@ export const create_paramter_api = `${environment.baseUrl}reports/create_paramet
 export const delete_parameter_api = `${environment.baseUrl}reports/delete_parameters/`
 
 //method post
-// export const get_pivot_table_data = `${environment.baseUrl}reports/get_pivot_data/`
-export const get_pivot_table_data = `https://ddm1.apps.pcfepg2wi.gm.com/reports/get_pivot_data/`
+export const get_pivot_table_data = `${environment.baseUrl}reports/get_pivot_data/`
+// export const get_pivot_table_data = `https://ddm1.apps.pcfepg2wi.gm.com/reports/get_pivot_data/`

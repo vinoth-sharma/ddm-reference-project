8b95e96b1c462e78ed586f4315d668a60b2c7563
Sahana P <sonal.raut@gm.com>
2020-01-21 15:10:44 +0530

    description bug fixes

--- src/app/semantic-new/semantic-new.component.ts
+++ src/app/semantic-new/semantic-new.component.ts
@@ -398,6 +398,10 @@ export class SemanticNewComponent {
         this.selectedTablesExisting = [];
         this.selectedTablesNonExisting = [];
         this.finalCustomTablesObjectArray = [];
+        this.authenticationService.getSldetails(this.userId).subscribe((res) => {
+          this.semanticList = res['data']['sl_list'];
+          this.semanticNewService.dataMethod(this.semanticList);
+        })
       },
       error => {
         this.toastrService.error(error['message']['error']);
@@ -572,6 +576,10 @@ public onDeSelectAllCustomNonExisting(event?:any) {
       this.data['sl_table_ids'] = this.tablesCombined;
       this.data['custom_table_ids'] = this.selectedTablesCustom;
       this.data['description']= this.descriptionField;
+      this.authenticationService.getSldetails(this.userId).subscribe((res) => {
+        this.semanticList = res['data']['sl_list'];
+        this.semanticNewService.dataMethod(this.semanticList);
+      })
       // // console.log("SUBMITTING TOTAL DATA:",this.data)
       this.checkEmpty();
     }

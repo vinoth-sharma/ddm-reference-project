dac74a7b198cf71da47af1ca4468ee31fb9ba292
bharath <bharath.s@mu-sigma.com>
2020-01-28 06:41:55 +0530

    RMP 435,476 fixed

--- src/app/rmp/Submit/select-report-criteria/select-report-criteria.component.ts
+++ src/app/rmp/Submit/select-report-criteria/select-report-criteria.component.ts
@@ -1055,7 +1055,7 @@ export class SelectReportCriteriaComponent implements OnInit {
           this.jsonfinal.select_frequency.push(frequencyDatas);
           this.jsonUpdate['select_frequency'].push(frequencyDatas);
         }
-
+        
         this.django.ddm_rmp_report_market_selection(this.select_report_selection).subscribe(response => {
           if (response["message"] == "success") {
             this.report_id_service.changeUpdate(true)
@@ -1417,10 +1417,23 @@ export class SelectReportCriteriaComponent implements OnInit {
       if (frequencyType.length && frequencyType === 'On Demand') {
         this.jsonfinal['frequency'] = "On Demand"
         this.jsonUpdate['frequency'] = "On Demand"
+        let dat = this.jsonUpdate.select_frequency.filter( item => item.ddm_rmp_lookup_select_frequency_id == subelement.ddm_rmp_lookup_select_frequency_id)
+        if(dat.length ==0){
+          var frequencyData = { "ddm_rmp_lookup_select_frequency_id": subelement.ddm_rmp_lookup_select_frequency_id, "description": "" };
+          this.jsonfinal.select_frequency.push(frequencyData);
+          this.jsonUpdate['select_frequency'].push(frequencyData);
+        }
+     
       }
       else if (frequencyType.length && frequencyType === 'On Demand Configurable') {
         this.jsonfinal['frequency'] = "On Demand Configurable"
         this.jsonUpdate['frequency'] = "On Demand Configurable"
+        let dat = this.jsonUpdate.select_frequency.filter( item => item.ddm_rmp_lookup_select_frequency_id == subelement.ddm_rmp_lookup_select_frequency_id)
+        if(dat.length ==0){
+          var frequency_Data = { "ddm_rmp_lookup_select_frequency_id": subelement.ddm_rmp_lookup_select_frequency_id, "description": "" };
+        this.jsonfinal.select_frequency.push(frequency_Data);
+        this.jsonUpdate['select_frequency'].push(frequency_Data);
+        }
       }
       else {
         this.jsonfinal['frequency'] = "Recurring"
@@ -1428,5 +1441,7 @@ export class SelectReportCriteriaComponent implements OnInit {
       }
     }
   }
+
+  
 
 }

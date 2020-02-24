be448fecf9e9ce2b061362d46e54be71365fd5bc
bharath <bharath.s@mu-sigma.com>
2020-02-25 00:22:20 +0530

    test cases for dependent modules

--- src/app/dependents-modal/dependents-modal.component.spec.ts
+++ src/app/dependents-modal/dependents-modal.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DependentsModalComponent } from './dependents-modal.component';
 
-describe('DependentsModalComponent', () => {
+fdescribe('DependentsModalComponent', () => {
   let component: DependentsModalComponent;
   let fixture: ComponentFixture<DependentsModalComponent>;
 
@@ -15,11 +15,25 @@ describe('DependentsModalComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DependentsModalComponent);
-    component = fixture.componentInstance;
+    component = fixture.debugElement.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show input data in the table',async(()=>{
+    fixture = TestBed.createComponent(DependentsModalComponent);
+    component = fixture.debugElement.componentInstance;
+    let reportData = [
+      {report_name:"name",created_by:"createdBy"}
+    ]
+    component.reports = reportData;
+    fixture.detectChanges();
+   let compiled = fixture.debugElement.nativeElement;
+  
+   expect(compiled.querySelector('tbody > tr').children[0].textContent).toContain('name');
+   expect(compiled.querySelector('tbody > tr').children[1].textContent).toContain('createdBy')
+  }))
 });

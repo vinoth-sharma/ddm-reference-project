7f7760109fb51f52a005573fb31d72d7fb60fa99
vinoth sharma <vinoth.veeramani@mu-sigma.com>
2020-02-28 20:43:13 +0530

    build bug fix

--- src/app/rmp/services/report-criteria-data.service.ts
+++ src/app/rmp/services/report-criteria-data.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class RepotCriteriaDataService {
+export class ReportCriteriaDataService {
 
   constructor() { }
    reportID=0;

b523adfed954586ac0e92c76ed867d2477444121
vinoth sharma <vinoth.veeramani@mu-sigma.com>
2020-03-02 21:35:57 +0530

    Testing - table container component - inprogress

--- src/app/report-manipulation/report/report-view/table-container/table-container.component.spec.ts
+++ src/app/report-manipulation/report/report-view/table-container/table-container.component.spec.ts
@@ -1,14 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TableContainerComponent } from './table-container.component';
+import { MaterialModule } from "../../../../material.module";
+import { CustomPipeModules } from "../../../../custom-directives/custom.pipes.module";
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ToastrService } from "ngx-toastr";
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('TableContainerComponent', () => {
+
+fdescribe('TableContainerComponent', () => {
   let component: TableContainerComponent;
   let fixture: ComponentFixture<TableContainerComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TableContainerComponent ]
+      declarations: [ TableContainerComponent ],
+      imports : [ MaterialModule,CustomPipeModules ,HttpClientTestingModule,NoopAnimationsModule],
+      providers: [
+        { provide: ToastrService, useValue: [] }
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +26,28 @@ describe('TableContainerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TableContainerComponent);
     component = fixture.componentInstance;
+    component.sheetData = {
+      tabs : [{tab_type: "table"} ]
+    }
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create Table Container Component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Test DateFormatter function', () => {
+    expect(component.dateFormattor(new Date())).toEqual("2-Mar-2020");
+  });
+
+  it('Test getOriginalColumnName function', () => {
+    component.customziedData = {
+      data : {
+        isCustomized : false
+      }
+    }
+    expect(component.getOriginalColumnName("column1")).toEqual("column1");
+  });
+
+
 });

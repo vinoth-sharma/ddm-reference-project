776c4f783deb124ecdc5f522477d4032d871d20f
vinoth sharma <vinoth.veeramani@mu-sigma.com>
2020-03-03 20:36:59 +0530

    toaster custom  component -  done

--- src/app/custom-directives/custom.pipes.module.ts
+++ src/app/custom-directives/custom.pipes.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { spaceFormaterString } from "./spaceFormaterString.pipe";
 import { ButtonCssDirective } from "./button-css.directive";
-
+import { NgToasterComponent } from "./ng-toaster/ng-toaster.component";
 @NgModule({
-    declarations : [spaceFormaterString,ButtonCssDirective],
-    exports : [spaceFormaterString,ButtonCssDirective]
+    declarations : [spaceFormaterString,ButtonCssDirective,NgToasterComponent],
+    exports : [spaceFormaterString,ButtonCssDirective,NgToasterComponent]
 })
 export class CustomPipeModules{
 

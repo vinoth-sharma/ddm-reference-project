8f4e3a145d71684bd3915bf08e466e5a1ec44ad7
vinoth sharma <vinoth.veeramani@mu-sigma.com>
2020-03-06 16:37:22 +0530

    removed unused serices - report view module

--- src/app/report-manipulation/report/report-routing.module.ts
+++ src/app/report-manipulation/report/report-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// import { HomeComponent } from './home/home.component';
 import { HomeComponent } from "./home/home.component";
-// import { HomeComponent } from './home/home.component';
 import { ReportContainerComponent } from './report-view/report-container/report-container.component'
 
 const routes: Routes = [

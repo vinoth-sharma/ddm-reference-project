3f2c81fbd5acf1566f8b954129ece84e5267d629
bharath <bharath.s@mu-sigma.com>
2020-03-10 07:45:14 +0530

    confirm modal : replaced fdscribe with describe

--- src/app/shared-components/confirm-modal/confirm-modal.component.spec.ts
+++ src/app/shared-components/confirm-modal/confirm-modal.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ConfirmModalComponent } from './confirm-modal.component';
 import { Component } from '@angular/core';
-fdescribe('ConfirmModalComponent', () => {
+describe('ConfirmModalComponent', () => {
   let component: ConfirmModalComponent;
   let fixture: ComponentFixture<ConfirmModalComponent>;
   let testHostFixture : ComponentFixture<MockParent>

01b0a435a831c1f7463e93d023afe55a51691615
Deepak Urs <deepak.urs@mu-sigma.com>
2020-03-26 17:18:54 +0530

    On-demand reports : Migration and unit test cases

--- src/app/rmp/custom-modals/ondemand-reports/ondemand-reports.component.ts
+++ src/app/rmp/custom-modals/ondemand-reports/ondemand-reports.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, EventEmitter, Output, Input,SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Output, Input, SimpleChanges } from '@angular/core';
 import { OndemandService } from '../ondemand.service';
-import { ToastrService } from "ngx-toastr";
-// import Utils from 'src/utils';
+import { NgToasterComponent } from "../../../custom-directives/ng-toaster/ng-toaster.component";
 declare var $: any;
 
 @Component({
@@ -10,61 +9,44 @@ declare var $: any;
   styleUrls: ['./ondemand-reports.component.css']
 })
 
+export class OndemandReportsComponent{
 
+  public onDemandScheduleId: any = '';
+  public odInfoObject: any;
+  public isLoading = true;
 
-export class OndemandReportsComponent implements OnInit {
-
-  public onDemandScheduleId:any;
-  public odcReportId:any;
-  public odcRequestNumber:any = '';
-  public odInfoObject:any;
-  public isLoading=true;
-
-
-  @Input() requestNumber:any;
-  @Input() reportId:any; 
-
+  @Input() requestNumber: any;
+  @Input() reportId: any;
   @Output() odScheduleConfirmation = new EventEmitter();
 
-  constructor(private onDemandService:OndemandService,
-              private toasterService: ToastrService) { }
+  constructor(private onDemandService: OndemandService, private toasterService: NgToasterComponent) { }
 
-  ngOnChanges(changes: SimpleChanges){
-    console.log("CHANGES SEEN IN OD:",changes)
-    // using the below service to get the schedule id
-    // if('reque')
-    // this.odcReportId = this.reportId;
-    if('requestNumber' in changes || 'reportId' in changes)
-    this.onDemandService.getOnDemandConfigDetails(changes.reportId.currentValue,changes.requestNumber.currentValue).subscribe(res=>{
-      console.log("NEW getOnDemandConfigDetails RESULTS",res); 
-      if(res["data"][1]['schedule_id'][0] || res["data"][1]['schedule_id'].length){
-        this.onDemandScheduleId = res["data"][1]['schedule_id'][0]
-      }
-      this.isLoading=false;
-      if(!this.onDemandScheduleId){
-        this.toasterService.error('Please ask the admin to configure scheduling parameters!');
-        $('#onDemandModal').modal('hide');
-        return;
-      }
-    })
-
-
-  }
-
-  ngOnInit() {
-    console.log("OD called only");
-
-  }
-
-  public startOnDemandSchedule(){
-    if(this.onDemandScheduleId){
-      this.odInfoObject = {confirmation:true,type:'On Demand Configurable',scheduleId:this.onDemandScheduleId,status:true}
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("CHANGES SEEN IN OD:", changes);
+    if ('requestNumber' in changes || 'reportId' in changes){
+      this.onDemandService.getOnDemandConfigDetails(changes.reportId.currentValue, changes.requestNumber.currentValue).subscribe(res => {
+        console.log("getOnDemandConfigDetails RESULTS", res);
+        if (res["data"][1]['schedule_id'][0] || res["data"][1]['schedule_id'].length) {
+          this.onDemandScheduleId = res["data"][1]['schedule_id'][0]
+        }
+        this.isLoading = false;
+        if (!this.onDemandScheduleId) {
+          this.toasterService.error('Please ask the admin to configure scheduling parameters!');
+          $('#onDemandModal').modal('hide');
+          return;
+        }
+      })
     }
-    else{
-      this.odInfoObject = {confirmation:true,type:'On Demand Configurable',scheduleId:this.onDemandScheduleId,status:false}
+  }
+
+  public startOnDemandSchedule() {
+    if (this.onDemandScheduleId) {
+      this.odInfoObject = { confirmation: true, type: 'On Demand', scheduleId: this.onDemandScheduleId, status: true }
+    }
+    else {
+      this.odInfoObject = { confirmation: false, type: 'On Demand', scheduleId: this.onDemandScheduleId, status: false }
     }
     this.odScheduleConfirmation.emit(this.odInfoObject);
-    
   }
 
 }

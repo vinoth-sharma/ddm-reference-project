799ef60ea2a20f30fb3c7aa380115ece440ede2c
vinoth sharma <vinoth.veeramani@mu-sigma.com>
2020-04-11 21:58:59 +0530

    house keeping

--- src/app/share-reports/share-reports.component.spec.ts
+++ src/app/share-reports/share-reports.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShareReportsComponent } from './share-reports.component';
 
-xdescribe('ShareReportsComponent', () => {
+describe('ShareReportsComponent', () => {
   let component: ShareReportsComponent;
   let fixture: ComponentFixture<ShareReportsComponent>;
 

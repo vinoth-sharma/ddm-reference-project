e5f44315354e9741c3b5ca3c1f467a50e6529df5
Deepak Urs <deepak.urs@mu-sigma.com>
2020-04-20 14:54:11 +0530

    app-routing cleaned for rmp-standalone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RolesAndResponsibilitiesHomeComponent } from "./roles-and-responsibilities-home/roles-and-responsibilities-home.component";
-import { RMPModule } from "./rmp/rmp.module";
 import { AuthGuard } from "./auth.guard";
-import { RMP_Routes } from "./rmp/rmp-routing.module"
 
 const routes: Routes = [
   {
     path: "user",
-    loadChildren : ()=>import("./rmp/rmp.module").then(m=>m.RMPModule),
+    loadChildren: () => import("./rmp/rmp.module").then(m => m.RMPModule),
     canActivate: [AuthGuard]
   },
   {
     path: "",
-    loadChildren : ()=>import("./rmp/rmp.module").then(m=>m.RMPModule)
-    // canActivate: [AuthGuard]
+    loadChildren: () => import("./rmp/rmp.module").then(m => m.RMPModule)
   },
   {
     path: "roles",
     component: RolesAndResponsibilitiesHomeComponent,
     canActivate: [AuthGuard]
   },
-  { 
-    path: "**", 
-    redirectTo: "" 
+  {
+    path: "**",
+    redirectTo: ""
   }
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 

9d23dc64dd9cadc37fff8b76d7f0c3173607a678
bharath <bharath.s@mu-sigma.com>
2020-04-24 21:03:42 +0530

    RMP-554 : Fixed

--- src/app/rmp/Main/ddm-admin/ddm-admin.component.ts
+++ src/app/rmp/Main/ddm-admin/ddm-admin.component.ts
@@ -230,6 +230,11 @@ export class DdmAdminComponent implements OnInit {
   }
 
   addDocument() {
+    this.document_details = {
+      "title": "",
+      "url": "",
+      "admin_flag": false
+    }
     let link_title = (<HTMLInputElement>document.getElementById('document-name')).value.toString();
     let link_url = (<HTMLInputElement>document.getElementById('document-url')).value.toString();
     let upload_doc = (<HTMLInputElement>document.getElementById("attach-file1")).files[0];

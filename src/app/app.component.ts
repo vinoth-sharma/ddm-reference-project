45d06e51ac326fe7b81504748ed7e118c181da46
vinoth sharma <vinoth.veeramani@mu-sigma.com>
2020-05-05 18:29:02 +0530

    redirect to home - when refresh the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,50 @@
 import { Component } from "@angular/core";
 import { DataProviderService } from "src/app/rmp/data-provider.service";
+import { Router } from '@angular/router';
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "DDM";
+  // title = "DDM";
 
-  associates;
-  firstName;
-  userData;
-  lastName;
-  roleId;
-  roleName;
-  dataLoad = false;
+  // associates;
+  // firstName;
+  // userData;
+  // lastName;
+  // roleId;
+  // roleName;
+  // dataLoad = false;
 
-  public spinnerConfig: any = {
-    bdColor: "rgba(51,51,51,0.8)",
-    size: "default",
-    color: "#fff",
-    type: "line-spin-clockwise-fade"
-  };
+  // public spinnerConfig: any = {
+  //   bdColor: "rgba(51,51,51,0.8)",
+  //   size: "default",
+  //   color: "#fff",
+  //   type: "line-spin-clockwise-fade"
+  // };
 
-  constructor(private dataProvider : DataProviderService) {
-    this.associates = [
-      {
-        role: "Admin",
-        firstname: "Jacquelin",
-        lastname: "Beiter"
-      },
-      { role: "Non-Admin", firstname: "Aubrey", lastname: "Dubberke" },
-      { role: "Report Viewer", firstname: "Charlie", lastname: "Chevoor" }
-    ];
-    this.dataProvider.currentIntialLoad.subscribe(element => {
-      if (element) {
-        this.dataLoad = true
-      }
-    })
+  constructor(private dataProvider : DataProviderService , public router: Router) {
+    // this.associates = [
+    //   {
+    //     role: "Admin",
+    //     firstname: "Jacquelin",
+    //     lastname: "Beiter"
+    //   },
+    //   { role: "Non-Admin", firstname: "Aubrey", lastname: "Dubberke" },
+    //   { role: "Report Viewer", firstname: "Charlie", lastname: "Chevoor" }
+    // ];
+    // this.dataProvider.currentIntialLoad.subscribe(element => {
+    //   if (element) {
+    //     this.dataLoad = true
+    //   }
+    // })
+  }
+  ngOnInit(){
+    this.router.navigate([''])
   }
 
-  recieveUserdetails($event) {
-    this.roleId = $event;
-  }
+  // recieveUserdetails($event) {
+  //   this.roleId = $event;
+  // }
 }

f48bbd4f58f77ff8ad756a9be81703e30bb6cbcb
vinoth sharma <vinoth.veeramani@mu-sigma.com>
2020-05-09 18:50:26 +0530

    menu - migrated to header

--- src/app/custom-directives/common.module.ts
+++ src/app/custom-directives/common.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
 import { MaterialModule } from "../material.module";
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
     declarations : [ HeaderComponent, FooterComponent ],
     providers:[],
-    imports : [ MaterialModule ],
+    imports : [ MaterialModule,RouterModule ],
     exports : [  HeaderComponent, FooterComponent ]
 })
 export class CommonModuleDdmRmp {
--- src/app/rmp/rmp-routing.module.ts
+++ src/app/rmp/rmp-routing.module.ts
@@ -14,32 +14,37 @@ import { ReportsComponent } from './reports/reports.component';
 import { MetricsComponent } from './metrics/metrics.component';
 import { DealerAllocationComponent } from './Submit/dealer-allocation/dealer-allocation.component';
 import { OrderToSaleComponent } from './Submit/order-to-sale/order-to-sale.component';
-import { SelectReportCriteriaComponent} from './Submit/select-report-criteria/select-report-criteria.component';
+import { SelectReportCriteriaComponent } from './Submit/select-report-criteria/select-report-criteria.component';
 import { SubmitLandingPageComponent } from './Submit/submit-landing-page/submit-landing-page.component';
 
 export const RMP_Routes = [
-  {path:'',component:RmpLandingPageComponent,children:[
-    {path:'main',component:MainMenuComponent,
-      children:[
-          {path:'home', component: MainMenuLandingPageComponent},
-          {path:'ddm',component:DdmIntroComponent},
-          {path:'user-profile',component:UserProfileComponent},
-          {path:'ddm-team',component:DdmTeamComponent},
-          {path:'reference-documents',component:ReferenceDocComponent},
-          {path:'ddm-admin',component:DdmAdminComponent},
-          {path: '', redirectTo: '/user/main/home'}
-      ]},
-      {path:'submit-request',component:SubmitRequestComponent,
-      children:[
-          {path:'select-report-criteria',component:SelectReportCriteriaComponent},
-          {path:'dealer-allocation',component:DealerAllocationComponent},
-          {path:'order-to-sale',component:OrderToSaleComponent},
-          {path:'', component:SubmitLandingPageComponent} 
-      ]},
-      {path:'request-status',component:RequestStatusComponent},
-      {path:'reports',component:ReportsComponent},
-      {path:'metrics',component:MetricsComponent},
-      {path:'', redirectTo:'main',pathMatch:'full'} 
+  {
+    path: '', component: RmpLandingPageComponent, children: [
+      {
+        path: 'main', component: MainMenuComponent,
+        children: [
+          { path: 'home', component: MainMenuLandingPageComponent },
+          { path: 'ddm', component: DdmIntroComponent },
+          { path: 'user-profile', component: UserProfileComponent },
+          { path: 'ddm-team', component: DdmTeamComponent },
+          { path: 'reference-documents', component: ReferenceDocComponent },
+          { path: 'ddm-admin', component: DdmAdminComponent },
+          { path: '', redirectTo: '/user/main/home' }
+        ]
+      },
+      {
+        path: 'submit-request', component: SubmitRequestComponent,
+        children: [
+          { path: 'select-report-criteria', component: SelectReportCriteriaComponent },
+          { path: 'dealer-allocation', component: DealerAllocationComponent },
+          { path: 'order-to-sale', component: OrderToSaleComponent },
+          { path: '', component: SubmitLandingPageComponent }
+        ]
+      },
+      { path: 'request-status', component: RequestStatusComponent },
+      { path: 'reports', component: ReportsComponent },
+      { path: 'metrics', component: MetricsComponent },
+      { path: '', redirectTo: 'main', pathMatch: 'full' }
     ]
   }];
 

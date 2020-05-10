8d68a87f8667294b8a92d6d861792d49b63fb4f1
vinoth sharma <vinoth.veeramani@mu-sigma.com>
2020-05-10 13:29:36 +0530

    submit request - help desc - integration done

--- src/app/rmp/submit-request/disclaimer-help-modal/disclaimer-help-modal.component.ts
+++ src/app/rmp/submit-request/disclaimer-help-modal/disclaimer-help-modal.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { SubmitRequestService } from "../submit-request.service";
+import { NgToasterComponent } from 'src/app/custom-directives/ng-toaster/ng-toaster.component';
+import { DjangoService } from '../../django.service';
+import Utils from '../../../../utils';
 
 @Component({
   selector: 'app-disclaimer-help-modal',
@@ -7,7 +11,6 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./disclaimer-help-modal.component.css']
 })
 export class DisclaimerHelpModalComponent implements OnInit {
-  public disclaimerHelpDesc = "";
   public config = {
     toolbar: [
       ['bold', 'italic', 'underline', 'strike'],
@@ -24,17 +27,43 @@ export class DisclaimerHelpModalComponent implements OnInit {
     ]
   };
 
+  l_lookupTableData:any = {};
+  public showEditOption:boolean = true;
+
+  public submitReqHelpDescObj = {
+    ddm_rmp_desc_text_id : 14,
+    module_name : "Help_SubmitRequest",
+    description : ""
+  }
 
   constructor(public dialogRef: MatDialogRef<DisclaimerHelpModalComponent>,
+    public subReqService: SubmitRequestService,
+    private toaster: NgToasterComponent,
+    private django: DjangoService,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
     // console.log(this.data);
+   this.l_lookupTableData = this.subReqService.getLookUpTableData();
+   this.l_lookupTableData.desc_text.forEach(element => {
+    if(element.ddm_rmp_desc_text_id === 14)
+     this.submitReqHelpDescObj.description = element.description
+  });
+  }
+
+  submitReqHelpDesc(){
+    Utils.showSpinner();
+    this.django.ddm_rmp_landing_page_desc_text_put(this.submitReqHelpDescObj).subscribe((response:any) => {
+      Utils.hideSpinner()
+      this.toaster.success(response.message);
+    }, err => {
+      Utils.hideSpinner()
+      this.toaster.error("Server Error");
+    })
   }
 
   textChanged(event){
-    console.log(event);
-    
+    // console.log(event);
   }
 
   closeDailog(): void {

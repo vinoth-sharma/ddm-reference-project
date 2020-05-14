f11eb1599662349df51f990b6b66a410b5eb227c
Deepak Urs <deepak.urs@mu-sigma.com>
2020-05-14 15:17:42 +0530

    minor changes to auth and app.module

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs';
-import { first } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { environment } from "../environments/environment";
 
@@ -8,71 +7,58 @@ import { environment } from "../environments/environment";
   providedIn: 'root'
 })
 export class AuthenticationService {
-
   public userid;
   public isButton: boolean;
-  public slid;
   public slDetails;
   public routeValue: boolean;
   schema: string;
+  private isUserLoggedIn;
 
   myMethod$: Observable<any>;
   Method$: Observable<any>;
   errorMethod$: Observable<any>;
-  button$: Observable<any>;
-  sl$: Observable<any>;
-  slRoute$: Observable<any>;
-  public userId: any = {};
   private myMethodSubject = new BehaviorSubject<any>("");
   public isButtonSubject = new BehaviorSubject<any>(this.isButton);
   public slMethodSubject = new BehaviorSubject<any>(this.userid);
   public slNamesMethodSubject = new BehaviorSubject<any>(this.slDetails);
   public slRouteValueSubject = new BehaviorSubject<any>(this.routeValue);
-  private isUserLoggedIn;
+
+  private errorMethodSubject = new BehaviorSubject<any>("")
+
   constructor(private http: HttpClient) {
     this.isUserLoggedIn = false;
     this.myMethod$ = this.myMethodSubject.asObservable();
     this.Method$ = this.slMethodSubject.asObservable();
-    this.button$ = this.isButtonSubject.asObservable();
     this.errorMethod$ = this.errorMethodSubject.asObservable();
-    this.sl$ = this.slNamesMethodSubject.asObservable();
-    this.slRoute$ = this.slRouteValueSubject.asObservable();
   }
-  private errorMethodSubject = new BehaviorSubject<any>("")
-  myMethod(userInformation, userid, schema) {
+
+  public myMethod(userInformation, userid, schema) {
     this.myMethodSubject.next(userInformation);
     this.slMethodSubject.next(userid);
     this.setSchema(schema);
   }
 
-
-  setSchema(schema) {
+  public setSchema(schema) {
     this.schema = schema;
   }
 
-  getSchema() {
-    return this.schema;
-  }
-  button(isButton) {
+  public button(isButton) {
     this.isButtonSubject.next(isButton);
   }
-  
-  errorMethod(userInformation) {
+  public errorMethod(userInformation) {
     this.errorMethodSubject.next(userInformation);
   }
-  SetUserDetails(){
-    this.isUserLoggedIn=true;
+  public SetUserDetails() {
+    this.isUserLoggedIn = true;
   }
 
-    getUserDetails(){
-      return this.isUserLoggedIn;
-    };
+  public getUserDetails() {
+    return this.isUserLoggedIn;
+  };
 
-  getHelpRedirection(value: string) {
+  public getHelpRedirection(value: string) {
     const serviceUrl = `${environment.baseUrl}login/help?help_section=${value}`;
     return this.http.get(serviceUrl, { responseType: 'blob' })
   }
 
 }
-
-
